f5e5da8beb55c62e59347e470e30192dde3645ac
Kyeongho <khjo1@suresofttech.com>
2023-01-25 17:35:30 +0900

    #8 Entity 객체 구성
    - 설계된 도메인을 토대로 Entity 객체 구성

---
+++ jpa-example-source/src/main/java/com/example/jpa/source/domain/Member.java
@@ -0,0 +1,88 @@
+package com.example.jpa.source.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * 회원 도메인
+ * 회원은 이름, 주소, 가입 일자 정보를 지니고 있다.
+ */
+@Entity
+@Table(name = "MEMBER")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(length = 500)
+    private String address;
+    @Column(nullable = false)
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date registedDate;
+
+    protected Member() {
+
+    }
+    private Member(String name, String address, Date registedDate) {
+        this.name = name;
+        this.address = address;
+        this.registedDate = registedDate;
+    }
+
+    public static Member of(String name, String address) {
+        return new Member(name, address, new Date());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getRegistedDate() {
+        return registedDate;
+    }
+
+    public void setRegistedDate(Date registedDate) {
+        this.registedDate = registedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id) && Objects.equals(name, member.name) && Objects.equals(address, member.address) && Objects.equals(registedDate, member.registedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, registedDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", registedDate=" + registedDate +
+                '}';
+    }
+}
---
+++ jpa-example-source/src/main/java/com/example/jpa/source/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.example.jpa.source.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
