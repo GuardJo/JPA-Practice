56dac84915876fb8673c1be093aaf95cd2f6eced
Kyeongho <khjo1@suresofttech.com>
2023-10-04 13:23:03 +0900

    #24 프록시 객체를 통한 연관관계 매핑
    - 프록시 객체를 통해 별도 select 쿼리 없이 연관관계 매핑하기
    -- 식별자 키 조회 시에는 프록시 객체 초기화가 일어나지 않음

--- jpa-practice/src/main/java/com/practice/jpa/chapter08/DataLoadExample.java
+++ jpa-practice/src/main/java/com/practice/jpa/chapter08/DataLoadExample.java
@@ -22,6 +22,10 @@ public class DataLoadExample implements Runnable {
 		printMemberAndTeam();
 		printMemberProxy();
 		printMemberAndTeamProxy();
+		printMemberIdProxy();
+
+		initNewTeamAndMember();
+		setNewRelationshipWithProxy();
 	}
 
 	private void initData() {
@@ -36,7 +40,20 @@ public class DataLoadExample implements Runnable {
 		transaction.commit();
 	}
 
+	private void initNewTeamAndMember() {
+		Team8 team = Team8.of("New Team");
+		Member8 member = Member8.of("New Member");
+
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(team);
+		entityManager.persist(member);
+		transaction.commit();
+	}
+
 	private void printMemberAndTeam() {
+		System.out.println("단일 Entity 및 연관관계 조회");
+
 		Member8 member = entityManager.find(Member8.class, 1L);
 
 		System.out.println(member);
@@ -44,27 +61,56 @@ public class DataLoadExample implements Runnable {
 	}
 
 	private void printMember() {
+		System.out.println("단일 Entity 조회");
+
 		Member8 member = entityManager.find(Member8.class, 1L);
 
 		System.out.println(member);
 	}
 
 	private void printMemberProxy() {
+		System.out.println("프록시 객체 조회");
+
 		entityManager.clear();
 
 		Member8 member8 = entityManager.getReference(Member8.class, 1L);
 
-		System.out.println(member8);
+		System.out.println(member8.getName());
 	}
 
 	private void printMemberAndTeamProxy() {
+		System.out.println("프록시 객체 및 연관관계 조회");
+
 		entityManager.clear();
 
 		Member8 member = entityManager.getReference(Member8.class, 1L);
 
-		System.out.println(member);
-
-		entityManager.clear(); // Persistence Context를 비움으로써 Member Entity를 준영속 상태로 전환
+		System.out.println(member.getName());
 		System.out.println(member.getTeam());
+	}
+
+	private void printMemberIdProxy() {
+		System.out.println("프록시 객체의 식별자값 조회 요청");
+
+		entityManager.clear();
+
+		Member8 member = entityManager.getReference(Member8.class, 1L);
+
+		System.out.println(member.getId());
+	}
+
+	private void setNewRelationshipWithProxy() {
+		System.out.println("프록시 객체를 활용한 연관관계 매핑");
+
+		entityManager.clear();
+
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+
+		Member8 member = entityManager.find(Member8.class, 2L);
+		Team8 team = entityManager.getReference(Team8.class, 2L);
+
+		member.setTeam(team);
+		transaction.commit();
 	}
 }
