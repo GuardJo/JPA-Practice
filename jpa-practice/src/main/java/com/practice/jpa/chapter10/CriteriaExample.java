d722914bdc46c7a849a61a44b533024e5cab9341
Kyeongho <khjo1@suresofttech.com>
2023-12-22 16:28:08 +0900

    #32 반환 타입별 CriteriaQuery 요청
    - 반환타입 지정 및 미지정 시에 따른 CriteriaQuery 생성 및 jpql 반환 구성

--- jpa-practice/src/main/java/com/practice/jpa/chapter10/CriteriaExample.java
+++ jpa-practice/src/main/java/com/practice/jpa/chapter10/CriteriaExample.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -23,6 +24,7 @@ public class CriteriaExample implements Runnable {
 	@Override
 	public void run() {
 		searchAllMemberWithCriteria();
+		searchAllObjectWithCriteria();
 		searchMemberOnlyAdult();
 	}
 
@@ -40,6 +42,21 @@ public class CriteriaExample implements Runnable {
 		members.forEach(System.out::println);
 	}
 
+	private void searchAllObjectWithCriteria() {
+		System.out.println("Criteria Builder 를 통한 Criteria Query 구성 및 Object 객체 조회");
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Object> criteriaQuery = criteriaBuilder.createQuery(); // 반환 타입을 지정하지 않을 경우 Object 로 반환
+
+		Root<Member10_2> m = criteriaQuery.from(Member10_2.class);
+		criteriaQuery.select(m);
+
+		Query query = entityManager.createQuery(criteriaQuery);
+
+		List<Object> objects = query.getResultList();
+
+		objects.forEach(System.out::println);
+	}
+
 	private void searchMemberOnlyAdult() {
 		System.out.println("Criteria를 통한 Member 조회 중 20세 이상인 Member들만 조회");
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
@@ -48,7 +65,7 @@ public class CriteriaExample implements Runnable {
 		Root<Member10_2> m = criteriaQuery.from(Member10_2.class);
 		Predicate predicate = criteriaBuilder.ge(m.get("age"), 20);
 		Order order = criteriaBuilder.desc(m.get("age"));
-		criteriaQuery = criteriaQuery
+		criteriaQuery
 			.select(m)
 			.where(predicate)
 			.orderBy(order);
