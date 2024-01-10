73e32f81a5dcd149b4a2b82bdc75afb6b5dfa6e6
Kyeongho <khjo1@suresofttech.com>
2024-01-10 15:42:02 +0900

    #32 QueryDSL에서의 정렬, 페이징 및 집합 처리 구성
    - 정렬을 위한 orderBy() 함수 사용
    - 페이징 처리를 위한 offset(), limit() 함수 및 QueryModifiers(), restrict() 함수 사용
    - 집합 처리를 위한 groupBy() 함수 사용

--- jpa-practice/src/main/java/com/practice/jpa/chapter10/QueryDslExample.java
+++ jpa-practice/src/main/java/com/practice/jpa/chapter10/QueryDslExample.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+import com.mysema.query.QueryModifiers;
+import com.mysema.query.SearchResults;
 import com.mysema.query.jpa.impl.JPAQuery;
 import com.practice.jpa.chapter10.domain.Member10_2;
 import com.practice.jpa.chapter10.domain.QMember10_2;
@@ -20,6 +22,10 @@ public class QueryDslExample implements Runnable {
 	public void run() {
 		searchAllMembers();
 		searchByNameMembers();
+		searchByMembersOrderByAge();
+		searchByMembersOrderByAge2();
+		searchByTotalCountWithPagination();
+		groupByMembers();
 	}
 
 	private void searchAllMembers() {
@@ -47,5 +53,69 @@ public class QueryDslExample implements Runnable {
 			.list(qMember);
 
 		members.forEach(System.out::println);
+	}
+
+	private void searchByMembersOrderByAge() {
+		System.out.println("QueryDSL을 활용하여 Member10_2 목록을 나이 순으로 정렬 및 페이징처리 하여 반환");
+
+		JPAQuery jpaQuery = new JPAQuery(entityManager);
+		QMember10_2 qMember = QMember10_2.member10_2;
+
+		List<Member10_2> members = jpaQuery.from(qMember)
+			.offset(2)
+			.limit(2)
+			.orderBy(qMember.age.desc())
+			.list(qMember);
+
+		members.forEach(System.out::println);
+	}
+
+	private void searchByMembersOrderByAge2() {
+		System.out.println("QueryDSL을 활용하여 Member10_2 목록을 나이 순으로 정렬 및 페이징처리 하여 반환2");
+
+		JPAQuery jpaQuery = new JPAQuery(entityManager);
+		QMember10_2 qMember = QMember10_2.member10_2;
+		QueryModifiers queryModifiers = new QueryModifiers(2L, 2L); // limit, offset
+
+		List<Member10_2> members = jpaQuery.from(qMember)
+			.restrict(queryModifiers)
+			.orderBy(qMember.age.desc())
+			.list(qMember);
+
+		members.forEach(System.out::println);
+	}
+
+	private void searchByTotalCountWithPagination() {
+		System.out.println("QueryDSL을 활용하여 페이징 처리에 따른 전체 데이터 수 반환");
+
+		JPAQuery jpaQuery = new JPAQuery(entityManager);
+		QMember10_2 qMember = QMember10_2.member10_2;
+
+		SearchResults<Member10_2> searchResults = jpaQuery.from(qMember)
+			.offset(2)
+			.limit(2)
+			.orderBy(qMember.age.desc())
+			.listResults(qMember);
+
+		long offset = searchResults.getOffset();
+		long limit = searchResults.getLimit();
+		long total = searchResults.getTotal();
+		List<Member10_2> members = searchResults.getResults();
+
+		System.out.printf("offset : %d, limit : %d, total : %d\n", offset, limit, total);
+		members.forEach(System.out::println);
+	}
+
+	private void groupByMembers() {
+		System.out.println("QueryDSL을 활용하여 팀별로 그룹핑 하여 팀별 평균 나이 조회");
+
+		JPAQuery jpaQuery = new JPAQuery(entityManager);
+		QMember10_2 qMember = QMember10_2.member10_2;
+
+		List<Double> avgAges = jpaQuery.from(qMember)
+			.groupBy(qMember.team)
+			.list(qMember.age.avg());
+
+		avgAges.forEach(avg -> System.out.printf("avg : %f\n", avg));
 	}
 }
